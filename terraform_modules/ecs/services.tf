15824f63dd900f04da81c7a372dbc3538d52e431
Miguel Matos <miguelamatos@ua.pt>
2023-12-17 01:15:40 +0000

    small fix

--- terraform_modules/ecs/services.tf
+++ terraform_modules/ecs/services.tf
@@ -1,6 +1,7 @@
 resource "aws_ecs_service" "user_ui_service" {
   name            = "user-ui-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.user_ui_task_definition.arn
   desired_count   = 1
 
   network_configuration {
@@ -18,17 +19,12 @@ resource "aws_ecs_service" "user_ui_service" {
     container_name   = "uachado-user-ui"
     container_port   = 80
   }
-
-  task_definition = replace(
-    aws_ecs_task_definition.user_ui_task_definition.arn,
-    "revision",
-    format("revision-%s", timestamp())
-  )
 }
 
 resource "aws_ecs_service" "inventory_api_service" {
   name            = "inventory-api-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.inventory_api_task_definition.arn
   desired_count   = 1
 
   network_configuration {
@@ -46,17 +42,12 @@ resource "aws_ecs_service" "inventory_api_service" {
     container_name   = "uachado-inventory-api"
     container_port   = 8000
   }
-
-  task_definition = replace(
-    aws_ecs_task_definition.inventory_api_task_definition.arn,
-    "revision",
-    format("revision-%s", timestamp())
-  )
 }
 
 resource "aws_ecs_service" "drop_off_points_api_service" {
   name            = "drop-off-points-api-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.drop_off_points_api_task_definition.arn
   desired_count   = 1
 
   network_configuration {
@@ -74,10 +65,4 @@ resource "aws_ecs_service" "drop_off_points_api_service" {
     container_name   = "uachado-drop-off-points-api"
     container_port   = 8000
   }
-
-  task_definition = replace(
-    aws_ecs_task_definition.drop_off_points_api_task_definition.arn,
-    "revision",
-    format("revision-%s", timestamp())
-  )
 }
